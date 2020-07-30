7b2cccb2be425fd9ddab9e80346e49e282bf121e
gitoqe <53040407+gitoqe@users.noreply.github.com>
2020-07-30 23:03:54 +0300

    Practice 1

--- 4.Practice.js
+++ 4.Practice.js
@@ -1,10 +1,28 @@
-Практические задачи
+/*
+        Практические задачи
 Задача №1
 Реализуйте функцию parseUrl(string), которая будет парсить URL строку и возвращать объект с распарсенными данными. Пример:
 
-let a = parseUrl('http://tutu.ru:8080/do/any.php?a=1&b[]=a&b[]=b#foo')
-
 // Вернет объект, в котором будут следующие свойства:
+
+Желательно задачу решить как можно меньшим числом строк кода и затратив на реализацию минимум времени.
+*/
+function parseUrl(string){
+	function a(string){
+		this.href = string;
+		this.hash = string.match(/#[a-z]*$/);
+		this.host = string.match(/[a-zA-Z0-9]*\.[a-zA-Z0-9]*:[0-9]{2,5}/);
+		this.port = (this.host[0]).match(/[0-9]+/);
+		this.protocol = string.match(/^[a-zA-Z]{3,5}:/);
+		this.origin = string.match(/^[a-z]*:\/\/[a-zA-Z0-9]*\.[a-z]*:[0-9]+/);
+		this.hostname = string.match(/[a-zA-Z0-9]*\.[a-z]*/);
+		this.pathname = string.match(/[/][a-z]+[/][a-zA-Z0-9]+\.[a-zA-Z0-9]+/);
+	}
+	var x = new a(string);
+	return x;
+}
+
+let a = parseUrl('http://tutu.ru:8080/do/any.php?a=1&b[]=a&b[]=b#foo')
 console.log( a.href == "http://tutu.ru:8080/do/any.php?a=1&b[]=a&b[]=b#foo" )
 console.log( a.hash == "#foo" )
 console.log( a.port == "8080" )
@@ -13,4 +31,3 @@ console.log( a.protocol == "http:" )
 console.log( a.hostname == "tutu.ru" )
 console.log( a.pathname == "/do/any.php" )
 console.log( a.origin == "http://tutu.ru:8080" )
-Желательно задачу решить как можно меньшим числом строк кода и затратив на реализацию минимум времени.
--- testing/done.js
+++ testing/done.js
@@ -227,3 +227,77 @@ console.log(myDrawRating(100), 100); // ★★★★★
 /*
 Что можно улучшить? Как бы вы переписали функцию drawRating при условии что на вход функции drawRating должна приходить переменная vote, содержащая значение от 0 до 100. Интересует именно логика реализации функции, не визуальное отображение звезд.
 */
+
+
+
+/*
+		Практические задачи
+Задача №1
+Реализуйте функцию parseUrl(string), которая будет парсить URL строку и возвращать объект с распарсенными данными. Пример:
+*/
+//let a = parseUrl('http://tutu.ru:8080/do/any.php?a=1&b[]=a&b[]=b#foo')
+
+// Вернет объект, в котором будут следующие свойства:
+/*
+console.log( a.href == "http://tutu.ru:8080/do/any.php?a=1&b[]=a&b[]=b#foo" )
+console.log( a.hash == "#foo" )
+console.log( a.port == "8080" )
+console.log( a.host == "tutu.ru:8080" )
+console.log( a.protocol == "http:" )
+console.log( a.hostname == "tutu.ru" )
+console.log( a.pathname == "/do/any.php" )
+console.log( a.origin == "http://tutu.ru:8080" )
+*/
+/*
+Желательно задачу решить как можно меньшим числом строк кода и затратив на реализацию минимум времени.
+*/
+function parseUrl(string){
+	function a(string){
+		/*
+		
+		^(.*:)//([A-Za-z0-9\-\.]+)(:[0-9]+)?(.*)$
+
+		proto $1
+		host $2
+		port $3
+		the-rest $4
+		*/
+		this.href = string;
+		this.hash = string.match(/#[a-z]*$/);
+		this.host = string.match(/[a-zA-Z0-9]*\.[a-zA-Z0-9]*:[0-9]{2,5}/)[0];
+		this.port = this.host.match(/[0-9]+/);
+		this.protocol = string.match(/^[a-zA-Z]{3,5}:/);
+		this.origin = string.match(/^[a-z]*:\/\/[a-zA-Z0-9]*\.[a-z]*:[0-9]+/);
+
+		this.hostname = string.match(/[a-zA-Z0-9]*\.[a-z]*/); //!
+		
+		this.pathname = string.match(/[/][a-z]+[/][a-zA-Z0-9]+\.[a-zA-Z0-9]+/);
+	}
+	var x = new a(string);
+	return x;
+}
+
+let my = parseUrl('http://tutu.ru:8080/do/any.php?a=1&b[]=a&b[]=b#foo');
+console.log(my.href);
+console.log(my.hash);
+console.log(my.port);
+console.log(my.host);
+console.log(my.protocol);
+console.log(my.hostname);
+console.log(my.pathname);
+console.log(my.origin);
+
+
+
+//проверка
+
+let a = parseUrl('http://tutu.ru:8080/do/any.php?a=1&b[]=a&b[]=b#foo');
+console.log(`\n\n\n\n___________test_____________`)
+console.log( a.href == "http://tutu.ru:8080/do/any.php?a=1&b[]=a&b[]=b#foo" )
+console.log( a.hash == "#foo" )
+console.log( a.port == "8080" )
+console.log( a.host == "tutu.ru:8080" )
+console.log( a.protocol == "http:" )
+console.log( a.hostname == "tutu.ru" )
+console.log( a.pathname == "/do/any.php" )
+console.log( a.origin == "http://tutu.ru:8080" )
